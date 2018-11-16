92cf8bccab83f5f4b2c89a255cc49967260ec1e6
Oween <34008762+gocrap@users.noreply.github.com>
2018-11-15 17:59:15 -0700

    Update Phi.java

--- Phi.java
+++ Phi.java
@@ -7,11 +7,11 @@ public class Phi{
 
     public static void main(String args[]){
 
-     BigDecimal n = new BigDecimal("1.0");
+                 BigDecimal n = new BigDecimal("1.0");
 		 
-     BigDecimal m = new BigDecimal("1.0");
+                 BigDecimal m = new BigDecimal("1.0");
 		 
-		 for (int i = 1; i < 1000000; i ++){
+		 for (int i = 1; i < 1000000; i ++){ // change 1000000 to the desired degree of accuracy
 		 	
 			 n = n.add(m);
 			 m = m.add(n);
@@ -23,10 +23,10 @@ public class Phi{
 		 
 		 
 
-	    System.out.println(m);
-		  System.out.println(n);
-	  	//m = m.divide(n , 100000, RoundingMode.HALF_UP);
-	  	//System.out.println(m);
+	          //System.out.println(m);
+		  //System.out.println(n);
+	  	  m = m.divide(n , 100000, RoundingMode.HALF_UP); // change 100000 to the number of decimals
+	  	  System.out.println(m);
 		 
     }
 }
